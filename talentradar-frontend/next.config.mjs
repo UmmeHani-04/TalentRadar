e18bf019673acaba1902a182fe1cc06dd183ea8b
Ummehani <ummehani.bscs@gmail.com>
2026-09-18 23:37:43 +0530

    Configure Next.js static export

--- talentradar-frontend/next.config.mjs
+++ talentradar-frontend/next.config.mjs
@@ -1,18 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/api/:path*',
-      },
-    ]
   },
 }
 
